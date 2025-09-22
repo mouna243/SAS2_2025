f819aca57d4342d138d9d1b212fa689711464d89
mouna243 <mounasaoui2108@gmail.com>
2025-09-22 17:11:21 +0200

    cha2

---
+++ D1/cha1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main (){
+    // Challenge 1 : Affichage Informations
+    printf ("entre le nom complet :");
+    printf ("entrer le sexe  :");
+    printf ("entrer le seail  :");
+    // Challenge 2 : Conversion de la température
+    float K ,C;
+    printf ("Entrer la température en Celsius : ");scanf ("%f", C);
+    K = C + 273.15;
+    printf("la température en Kelvin est : %f",K); 
+    //Challenge 3 : Conversion de la distance
+
+}
