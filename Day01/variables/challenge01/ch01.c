45c1cdc3f2c972c7981e10a8d7adbf8504ee9ddb
mouna243 <mounasaoui2108@gmail.com>
2025-09-23 15:30:00 +0200

    cha02

---
+++ Day01/variables/challenge01/ch01.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main (){
+   char a,b ;
+   printf("entrer un nomber : \n");
+   scanf("%s", &a);
+   printf("entrer le deusieme nomber : \n");
+   scanf("%s", &b);
+   //
+   if (a == b){
+       printf("le triple de leur somme est : ")
+    }
+
+
+return 0;
+}
