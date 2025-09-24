aff3f0bf6b2eef7750dc1bd43f680036feea50f5
mouna243 <mounasaoui2108@gmail.com>
2025-09-24 18:20:51 +0200

    challange

--- Day02/mini_project/miniprojet.c
+++ Day02/mini_project/miniprojet.c
@@ -1,1 +1,32 @@
 //without fonction 
+#include <stdio.h>
+#include <stdlib.h>
+int main() {
+ printf("combien des livres trouver " , &m);
+int m ;
+   do {
+    char titre [20] , auteur [20];
+    float prix;
+    int quantite ;
+    
+    printf ("Entrer le titre de livre : ");
+    scanf ("%s" ,&titre);
+    printf ("Entrer l'auteur du livre. : ");
+    scanf ("%s" , &auteur);
+    printf ("Entrer le prix du livre : ");
+    scanf ("%f", &prix);
+    printf ("Entrer Quantité en stock: ");
+    scanf ("%d", &quantite);
+    
+  if (quantite != 0) {
+      printf("On'a %d livre qui s'apelle %s et écrit par %s et sont prix = %.2f dh \n", quantite, prix , auteur, titre);
+  }else {
+      printf("On'a %d livre qui s'apelle %s et écrit par %s et sont prix = %f dh ", quantite, prix , auteur, titre);
+  }
+    printf("si tu veux sortie taper 0 si non taper quellque nomber :");
+    scanf("%d ",&m ); 
+    
+    }while();
+    return 0;
+
+}
